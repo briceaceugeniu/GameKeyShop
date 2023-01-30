adb7ed4b0d469370115096e9157953eafd889845
Eugeniu <28739569+briceaceugeniu@users.noreply.github.com>
2023-01-30 22:29:16 +0100

    Seeding Product

--- GameKeyShop/Server/Data/DataContext.cs
+++ GameKeyShop/Server/Data/DataContext.cs
@@ -8,6 +8,36 @@ namespace GameKeyShop.Server.Data
             // empty ctor    
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "Prod 1",
+                    Description = "Description Prod 1",
+                    ImageUrl = "https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_960_720.jpg",
+                    Price = 1.3m
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Prod 2",
+                    Description = "Description Prod 2",
+                    ImageUrl = "https://cdn.pixabay.com/photo/2022/10/29/19/21/golden-eagle-7555959_960_720.jpg",
+                    Price = 3.5m
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Prod 3",
+                    Description = "Description Prod 3",
+                    ImageUrl = "https://cdn.pixabay.com/photo/2023/01/12/05/32/duck-7713310_960_720.jpg",
+                    Price = 2.4m
+                }
+            );
+        }
+
         public DbSet<Product> Products { get; set; }
     }
 }
---
+++ GameKeyShop/Server/Migrations/20230130212210_ProductSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GameKeyShop.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Description Prod 1", "https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_960_720.jpg", "Prod 1", 1.3m },
+                    { 2, "Description Prod 2", "https://cdn.pixabay.com/photo/2022/10/29/19/21/golden-eagle-7555959_960_720.jpg", "Prod 2", 3.5m },
+                    { 3, "Description Prod 3", "https://cdn.pixabay.com/photo/2023/01/12/05/32/duck-7713310_960_720.jpg", "Prod 3", 2.4m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
