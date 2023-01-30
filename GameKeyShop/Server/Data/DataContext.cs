1394c3022474144539636364e6b6fe17e78470b2
Eugeniu <28739569+briceaceugeniu@users.noreply.github.com>
2023-01-30 21:54:58 +0100

    create DbContext with DbSet for Product

---
+++ GameKeyShop/Server/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿
+namespace GameKeyShop.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            // empty ctor    
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
