b19ab17396b93824441e5cc6ea9c62ce21ff7e59
Eugeniu <28739569+briceaceugeniu@users.noreply.github.com>
2023-02-12 21:43:45 +0100

    server: admin category

--- GameKeyShop/Server/Controllers/CategoryController.cs
+++ GameKeyShop/Server/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace GameKeyShop.Server.Controllers
 {
@@ -18,6 +20,34 @@ namespace GameKeyShop.Server.Controllers
         {
             var category = await _service.GetCategoriesAsync();
             return Ok(category);
+        }
+
+        [HttpGet("admin"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> GetAdminCategories()
+        {
+            var result = await _service.GetAdminCategories();
+            return Ok(result);
+        }
+
+        [HttpDelete("admin/{id}"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> DeleteCategory(int id)
+        {
+            var result = await _service.DeleteCategory(id);
+            return Ok(result);
+        }
+
+        [HttpPost("admin"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> AddCategory(Category category)
+        {
+            var result = await _service.AddCategory(category);
+            return Ok(result);
+        }
+
+        [HttpPut("admin"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> UpdateCategory(Category category)
+        {
+            var result = await _service.UpdateCategory(category);
+            return Ok(result);
         }
     }
 }
