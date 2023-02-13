4db67c873b3d4b583e01db4d997956b15e05704a
Eugeniu <28739569+briceaceugeniu@users.noreply.github.com>
2023-02-13 08:26:02 +0100

    client: admin category

--- GameKeyShop/Client/Services/CategoryService/CategoryService.cs
+++ GameKeyShop/Client/Services/CategoryService/CategoryService.cs
@@ -10,6 +10,10 @@
         }
 
         public List<Category> Categories { get; set; } = new();
+        public List<Category> AdminCategories { get; set; } = new();
+
+        public event Action OnChange;
+
         public async Task GetCategoriesAsync()
         {
             var response = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/category");
@@ -17,6 +21,48 @@
             {
                 Categories = response.Data;
             }
+        }
+
+        public async Task AddCategory(Category category)
+        {
+            var response = await _http.PostAsJsonAsync("api/Category/admin", category);
+            AdminCategories = (await response.Content
+                .ReadFromJsonAsync<ServiceResponse<List<Category>>>()).Data;
+            await GetCategoriesAsync();
+            OnChange.Invoke();
+        }
+
+        public Category CreateNewCategory()
+        {
+            var newCategory = new Category { IsNew = true, Editing = true };
+            AdminCategories.Add(newCategory);
+            OnChange.Invoke();
+            return newCategory;
+        }
+
+        public async Task DeleteCategory(int categoryId)
+        {
+            var response = await _http.DeleteAsync($"api/Category/admin/{categoryId}");
+            AdminCategories = (await response.Content
+                .ReadFromJsonAsync<ServiceResponse<List<Category>>>()).Data;
+            await GetCategoriesAsync();
+            OnChange.Invoke();
+        }
+
+        public async Task UpdateCategory(Category category)
+        {
+            var response = await _http.PutAsJsonAsync("api/Category/admin", category);
+            AdminCategories = (await response.Content
+                .ReadFromJsonAsync<ServiceResponse<List<Category>>>()).Data;
+            await GetCategoriesAsync();
+            OnChange.Invoke();
+        }
+
+        public async Task GetAdminCategories()
+        {
+            var response = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/Category/admin");
+            if (response != null && response.Data != null)
+                AdminCategories = response.Data;
         }
     }
 }
--- GameKeyShop/Client/Services/CategoryService/ICategoryService.cs
+++ GameKeyShop/Client/Services/CategoryService/ICategoryService.cs
@@ -2,7 +2,14 @@
 {
     public interface ICategoryService
     {
+        event Action OnChange;
         List<Category> Categories { get; set; }
         Task GetCategoriesAsync();
+        List<Category> AdminCategories { get; set; }
+        Task GetAdminCategories();
+        Task AddCategory(Category category);
+        Task UpdateCategory(Category category);
+        Task DeleteCategory(int categoryId);
+        Category CreateNewCategory();
     }
 }
